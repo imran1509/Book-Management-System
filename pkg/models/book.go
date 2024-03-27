96d4a3841ee6c10318ae0ab34ca73048ca5cf5db
Mohd Imran <iimran2508@gmail.com>
2024-03-27 13:54:54 +0530

    added book.go file

---
+++ pkg/models/book.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/imran1509/book-management-system/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}

606d8e3ab822fbc0936a8e96152f46e17a19ed95
Mohd Imran <iimran2508@gmail.com>
2024-03-27 15:52:37 +0530

    added functions to  book.go file

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,27 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookByID(Id int64) (*Book, *gorm.DB) {
+	var GetBook Book
+	db := db.Where("ID=?", Id).Find(&GetBook)
+	return &GetBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
